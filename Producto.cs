943c65f2ce8dbac302ed0282e83576b22d557618
Tincho44 <martinharbuet@gmail.com>
2022-08-23 23:47:04 -0300

    Ahora si commiteamos la clase Producto y la clase Review

---
+++ Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class Producto
+    {
+
+        public string Nombre { get; }
+        public int Precio { get; set; }
+        public List<Review> AgregarReviews = new List<Review>();
+
+    }
+
+}
+
+
---
+++ Review.cs
@@ -0,0 +1,11 @@
+namespace Program
+{
+    public class Review
+    {
+
+        public int Calificacion {get; set; }
+        public string Comentario {get; set; }
+
+    }
+
+}
